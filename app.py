e1edc1b9c8ee67793c04a543fd234f0b80c77555
k-r-t <kritishrestha2062@gmail.com>
2026-04-21 16:46:03 +0545

    Refactor app.py for improved readability and structure

--- app.py
+++ app.py
@@ -26,10 +26,6 @@ st.markdown('<div class="header-box"><h1> Pharmacy Inventory Forecaster</h1></di
 def load_data():
     file_path = 'data/archive (2)/salesdaily.csv'
     df = pd.read_csv(file_path)
-        for col in ['Weekday Name', 'Unnamed: 0']:
-            if col in df.columns:
-                df = df.drop(columns=[col])
-    
     if 'datum' in df.columns:
         df['datum'] = pd.to_datetime(df['datum'], errors='coerce')
         df = df.dropna(subset=['datum'])
